0393ea98475f3a55ecbfc10a380179dc9b146b61
mhauzer <mhauzer@gmail.com>
2024-12-06 15:59:27 +0100

    dies Veneris

--- aspyre.py
+++ aspyre.py
@@ -1,4 +1,3 @@
-import json
 from aspyreengine import *
 from narrator import Narrator
 
@@ -7,99 +6,75 @@ from narrator import Narrator
 debug = False
 dev = True
 
-class LocationEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, Location):
-            return {
-                '__type__': 'Location', 
-                'name': obj.name, 
-                'description': obj.description, 
-                'exits': obj.exits,
-                'preposition': obj.preposition
-            }
-        return json.JSONEncoder.default(self, obj)
-    
-def complex_decoder(dct):
-    if '__type__' in dct:
-        if dct['__type__'] == 'Location':
-            return Location(dct['name'], dct['description'], dct['exits'], dct['preposition'])
-    return dct
+class Console:
+    __messages = []
+
+    def add_message(self, message):
+        self.__messages.append(message)
+
+    def get_message(self, prompt):
+        return input(prompt)
+
+    def print_messages(self):
+        for m in self.__messages:
+            print(m)
+        self.__messages = []
+
+    def print(self, message):
+        self.add_message(message)
+        self.print_messages()
 
 class Game:
     __LOCATIONS_FILE_NAME = 'locations.json'
     __CREATURES_FILE_NAME = 'creatures.json'
+    __ITEMS_FILE_NAME = 'items.json'
+    __SAVEGAME_FILE_NAME = 'savegame.json'
 
     engine = WorldEngine()
     narrator = Narrator()
-    resources_changed = False
-    messages = []
+    console = Console()
 
     def intro(self):
-        self.messages.append("Aspyre - Winter Adventure by Michał Hauzer, 2024")
-        self.messages.append('. * , . * . \' , ` . * . \' * .  ` . *  \' * , ` *.')
-        self.messages.append("")
-
-    def print_messages(self):
-        for m in self.messages:
-            print(m)
-        self.messages = []
+        self.console.add_message("Aspyre - Winter Adventure by Michał Hauzer, 2024")
+        self.console.add_message('. * , . * . \' , ` . * . \' * .  ` . *  \' * , ` *.')
+        self.console.add_message("")
 
     def set_name(self):
         while len(self.engine.player.name) == 0:            
-            self.engine.player.name = input("What is your name? ").strip()
-            print("")
-
-    def load_resources(self, filename):
-        if debug:
-            print(f"Loading resources from {filename}...")
-        with open(filename, 'r') as f:
-            # res = json.load(f, object_hook=complex_decoder)
-            # self.engine.locations = res['locations']
-            # if debug:
-            #     print(res['locations'])
-            #     print(self.locations[0].name)
-            locations = json.load(f, object_hook=complex_decoder)
-            for i in range(len(locations)):
-                self.engine.locations[i] = locations[f"{i}"]
-        f.close()        
-
-    def save_resources(self, filename):
-        print("Saving resources...")
-        f = open(filename, "w")
-        f.write(json.dumps(game.engine.locations, cls=LocationEncoder, indent=3))
-        f.close()
+            self.engine.player.name = self.console.get_message("What is your name? ").strip()
+            self.console.print("")
 
     def main_loop(self):
         end = False
 
         while not end:
-            if (self.engine.player.location_changed):                
-                location = self.engine.get_player_location()
-                self.messages.append(self.narrator.get_location_msg(location, detailed = True))
-                self.messages.append("")
-                self.messages.append(self.narrator.get_location_exits_msg(location, self.engine.locations))
+            if self.engine.player.location_changed:
+                location = self.engine.get_object_location(self.engine.player)
+                self.console.add_message(self.narrator.get_location_msg(location, detailed = True))
+                self.console.add_message("")
+                self.console.add_message(self.narrator.get_location_exits_msg(location, self.engine.get_locations()))
                 self.engine.player.location_changed = False
 
-            self.messages.append("")
-            self.print_messages()
+            self.console.add_message("")
+            self.console.print_messages()
             
             command = input(">")            
-            self.messages.append("")
-            self.print_messages()
+            self.console.add_message("")
+            self.console.print_messages()
     
             match(command.split()):
-                case ["s"] | ["w"] | ["e"] | ["n"]: self.messages.append(self.narrator.move_player(command, self.engine.move_player(command)))
-                case "go", y: self.messages.append(self.narrator.move_player(y, self.engine.move_player(y)))
-                case "go", "to", y: self.messages.append(self.narrator.move_player(y, self.engine.move_player(y)))
-                case "kill", x: self.messages.append(self.narrator.kill(self.kill(x)))
-                case "kill", "a" | "the", x: self.messages.append(self.narrator.kill(self.kill(x)))
+                case ["s"] | ["w"] | ["e"] | ["n"]: self.console.add_message(self.narrator.move_player(command, self.engine.move_object(self.engine.player, command)))
+                case "go", y: self.console.add_message(self.narrator.move_player(y, self.engine.move_player(y)))
+                case "go", "to", y: self.console.add_message(self.narrator.move_player(y, self.engine.move_player(y)))
+                case "kill", x: self.console.add_message(self.narrator.kill(self.kill(x)))
+                case "kill", "a" | "the", x: self.console.add_message(self.narrator.kill(self.kill(x)))
                 case "where", "am", "i": self.engine.player.location_changed = True
-                case ["wait"]: self.messages.append(self.narrator.wait())
+                case ["wait"]: self.console.add_message(self.narrator.wait())
                 case ["quit"] | ["q"]: end = True
-                case _: self.messages.append(self.narrator.unknown_command(command))
+                case _: self.console.add_message(self.narrator.unknown_command(command))
 
     def run(self):
-        self.load_resources(self.__LOCATIONS_FILE_NAME)
+        self.engine.load_resources(self.__LOCATIONS_FILE_NAME)
         self.intro()
 
         if not dev:
@@ -107,16 +82,19 @@ class Game:
         else:
             self.engine.player.name = 'Michał'
 
-        self.messages.append(self.narrator.get_hello_msg(self.engine.player.name))
-        self.messages.append("")
+        self.console.add_message(self.narrator.get_hello_msg(self.engine.player.name))
+        self.console.add_message("")
 
         self.main_loop()
 
-        if self.resources_changed:
-            game.save_resources(self.__LOCATIONS_FILE_NAME)
+        if self.engine.resources_changed:
+            self.engine.save_resources(self.__LOCATIONS_FILE_NAME)
 
-        self.messages.append(self.narrator.get_goodbye_msg(self.engine.player.name))
-        self.print_messages()        
-                
-game = Game()
-game.run()
+        self.console.add_message(self.narrator.get_goodbye_msg(self.engine.player.name))
+        self.console.print_messages()        
+
+def main():                
+    game = Game()
+    game.run()
+
+main()
--- aspyreengine.py
+++ aspyreengine.py
@@ -1,4 +1,6 @@
-class Creature:    
+import json
+
+class Creature:
     def __init__(self, species, name, health, strength, location_id, location_changed = False):
         self.species = species
         self.name = name
@@ -7,7 +9,7 @@ class Creature:
         self.location_id = location_id
         self.location_changed = location_changed
 
-class Location:
+class Location:    
     def __init__(self, name, description, exits, preposition):
         self.name = name
         self.description = description
@@ -15,19 +17,54 @@ class Location:
         self.preposition = preposition
 
 class WorldEngine:
+    resources_changed = False
+
     def __init__(self):
-        self.locations = {}
+        self.__locations = {}
         self.player = Creature("human", "", 100, 10, 0, True)
 
-    def get_player_location(self):
-        return self.locations[self.player.location_id]
+    def load_resources(self, filename):
+        with open(filename, 'r') as f:
+            locations = json.load(f, object_hook=complex_decoder)
 
-    def move_player(self, direction):        
-        if (not direction in self.locations[self.player.location_id].exits):
+        self.__locations = {}
+
+        for i in range(len(locations)):
+            self.__locations[i] = locations[f"{i}"]
+
+    def save_resources(self, filename):
+        with open(filename, "w") as f:
+            f.write(json.dumps(self.__locations, cls=LocationEncoder, indent=3))
+
+    def get_object_location(self, o):
+        return self.__locations.get(o.location_id)
+
+    def move_object(self, o, direction):        
+        if not direction in self.__locations[o.location_id].exits:
             return False
         
-        self.player.location_id = self.locations[self.player.location_id].exits[direction]
-        self.player.location_changed = True
+        o.location_id = self.__locations[o.location_id].exits[direction]
+        o.location_changed = True
 
-        return True        
+        return True
+    
+    def get_locations(self):
+        return self.__locations
             
+class LocationEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Location):
+            return {
+                '__type__': 'Location', 
+                'name': obj.name, 
+                'description': obj.description, 
+                'exits': obj.exits,
+                'preposition': obj.preposition
+            }
+        return json.JSONEncoder.default(self, obj)
+    
+def complex_decoder(dct):
+    if '__type__' in dct:
+        if dct['__type__'] == 'Location':
+            return Location(dct['name'], dct['description'], dct['exits'], dct['preposition'])
+    return dct
